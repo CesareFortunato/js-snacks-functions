27e422ad0c1fa59df374db5722afc759919d50f0
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-09 15:24:15 +0100

    feat: snack 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,43 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array 
+contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
+const character = "A";
 // Dichiara la funzione qui.
+
+//funzione tradizionale
+
+/* function buildArray(myArray, myChar){
+    const finalArray = [];
+    for (let i = 0; i < myArray.length; i++){
+        if (myArray[i][0] === myChar){
+            finalArray.push(myArray[i]);
+        }
+    }
+    return finalArray;
+
+} */
+
+//funzione arrow
+
+const buildArray = (myArray, myChar) => {
+    const finalArray = [];
+    for (let i = 0; i < myArray.length; i++){
+        if (myArray[i][0] === myChar){
+            finalArray.push(myArray[i]);
+        }
+    }
+    return finalArray;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const result = buildArray(names, character);
+
+console.log(result);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
