0f3b5f1230a38ce5fa1774a06d227b8a8f335f0a
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-09 15:44:36 +0100

    feat: snakc 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,31 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+//funzione tradizionale
+
+function checkVowels(myString) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
+    let stringVowels = [];
+
+    for (let i = 0; i < myString.length; i++) {
+        const char = myString[i].toLowerCase();
+        if (vowels.includes(char)) {       // se il carattere è una vocale
+            count++;                         // aumento il contatore
+            stringVowels.push(char);              // salvo la vocale trovata
+        }
+    }
+
+    return count+ " " + stringVowels;
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const result = checkVowels(word);
+
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
