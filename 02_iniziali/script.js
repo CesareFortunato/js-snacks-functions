42a5192638c6a95115e060f27e650f7e75b6ffba
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-09 14:54:30 +0100

    feat: snack 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,44 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni 
+parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
+
 // Dichiara la funzione qui.
+
+//funzione tradizionale
+
+/* function firstArray(myArray){
+    const resultArray = [];
+
+    for (let i = 0; i < myArray.length; i++) {
+  resultArray.push(myArray[i][0]);
+}
+
+return resultArray;
+
+} */
+
+
+//funzione arrow
+
+const firstArray = (myArray) => {
+    const resultArray = [];
+
+    for (let i = 0; i < myArray.length; i++) {
+  resultArray.push(myArray[i][0]);
+}
+
+return resultArray;
+
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const result = firstArray(names);
 
+console.log(result);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
